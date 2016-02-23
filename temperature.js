c651023198709d8c6dced86d9f4568c9bbb32cb9
Adrian <alu0100288216@ull.edu.es>
2016-02-23 13:04:16 +0000

    Modificada la expresión regular del fichero temperature.js y la sintaxis de la sentencia 'if'.

--- temperature.js
+++ temperature.js
@@ -2,15 +2,15 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /^\s*([-+]?\d+(?:\.\d*)?(?:e[+-]?\d+(?:\.\d*)?)?)\s*(f(?:a(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?|c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?)\s*$/i;
+
   var m = temp.match(regexp);
-  
+
   if (m) {
     var num = m[1];
     var type = m[2];
     num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
+    if (type[0] == 'c' || type[0] == 'C') {
       result = (num * 9/5)+32;
       result = result.toFixed(1)+" Farenheit"
     }
